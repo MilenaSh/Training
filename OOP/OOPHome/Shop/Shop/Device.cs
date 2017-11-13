d5f1041d00a8c2d38b430fd40095dc8578662beb
Milena Shishkova <shishkova.milena@gmail.com>
2017-11-14 00:56:01 +0200

    Added phone device shop

---
+++ OOP/OOPHome/Shop/Shop/Device.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Device
+    {
+        // empty constructor
+
+        public Device()
+        {
+
+        }
+
+        // constructor with all properties
+
+        public Device( string name, int price, bool inStock, DeviceType type)
+        {
+        this.Name = name;
+        this.Price = price;
+        this.InStock = inStock;
+        this.Type = type; // TOCHECK
+        }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public bool InStock { get; set; }
+
+        public DeviceType Type { get; set; }
+
+    public override string ToString()
+    {
+        return String.Format("Name: {0}, Price: {1}, InStock: {2}, Type: {3}", Name, Price, InStock, Type);
+    }
+
+}
+
---
+++ OOP/OOPHome/Shop/Shop/DeviceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Enum
+{
+    public enum DeviceType
+    {
+        Phone = 1,
+        Laptop = 2,
+        Tablet = 3
+    }
+
+
+}
