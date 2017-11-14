4821e3b969fc795849e8ce38293eabaf0d4905f7
MilenaSh <shishkova.milena@gmail.com>
2017-11-14 14:56:27 +0200

    Added CSS module

---
+++ CSSModule/CSSModule/CSSTool/ICSSService.cs
@@ -0,0 +1,18 @@
+﻿using CSSTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSTool
+{
+    public interface ICSSService
+    {
+        string MinimizeFile(string fileText);
+
+        string AddRulesToCss(string css, CSSRule[] rules);
+
+        CSSRule CreateRuleMode(string rule, Dictionary<string, string> values);
+    }
+}
