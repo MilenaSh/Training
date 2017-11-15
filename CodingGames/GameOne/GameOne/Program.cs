f1a9e4adc55396a609024a482b9a9ef3f3393336
MilenaSh <shishkova.milena@gmail.com>
2017-11-15 14:58:39 +0200

    Added DSA

--- CodingGames/GameOne/GameOne/Program.cs
+++ CodingGames/GameOne/GameOne/Program.cs
@@ -14,7 +14,10 @@ class Player
     static void Main(string[] args)
     {
         string[] inputs;
-
+        var slowDownDistance = 1200;
+        var stopDistance = 0;
+        var readyToBoost = false;
+        var count = 0;
         // game loop
         while (true)
         {
@@ -26,23 +29,40 @@ class Player
             int nextCheckpointDist = int.Parse(inputs[4]); // distance to the next checkpoint
             int nextCheckpointAngle = int.Parse(inputs[5]); // angle between your pod orientation and the direction of the next checkpoint
             inputs = Console.ReadLine().Split(' ');
-            int opponentX = int.Parse(inputs[0]);
-            int opponentY = int.Parse(inputs[1]);
+            int opponentX = int.Parse(inputs[6]);
+            int opponentY = int.Parse(inputs[7]);
 
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
 
             string thrust = "100";
 
-            if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90)
+            var ratio = (nextCheckpointDist - slowDownDistance + stopDistance) / slowDownDistance;
+
+
+            if ((nextCheckpointAngle > 90 || nextCheckpointAngle < -90) || nextCheckpointDist < 200)
             {
                 thrust = "0";
             }
 
-            if (nextCheckpointAngle == 0)
+            var dist = ((x - opponentX) ^ 2 + (y - opponentY) ^ 2) ^ (1 / 2);
+            if (dist <= 400)
+            {
+                thrust = "SHIELD";
+                readyToBoost = true;
+            }
+
+            if (readyToBoost)
             {
                 thrust = "BOOST";
+                readyToBoost = false;
             }
+
+            //   if (ratio <= 1 && stopDistance >= nextCheckpointDist)
+            //   {
+            //       thrust = ((int)(100.00 * ratio) + 40).ToString();
+            //   }       
+
 
             // You have to output the target position
             // followed by the power (0 <= thrust <= 100)
---
+++ DSA/DataStructures/DataStructures/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class MyList<T>
+    {
+        private const int InitialSize = 4;
+        private const int Multiplier = 2;
+
+        private int size;
+        private T[] arr;
+
+        public int Length { get; set; }
+
+
+        public MyList()
+        {
+            size = InitialSize;
+            arr = new T[size];
+            Length = 0;
+        }
+
+        //public void Add()
+
+
+    }
+}
